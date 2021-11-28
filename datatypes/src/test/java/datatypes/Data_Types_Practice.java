830fb6ab1ee554f7aa7a5c61fd0a704e293445d3
Shreyas <Shreyas@DESKTOP-T2COU3K.Home>
2021-11-28 11:36:52 +0000

    new commit for datatypes repo

---
+++ datatypes/src/test/java/datatypes/Data_Types_Practice.java
@@ -0,0 +1,29 @@
+package datatypes;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Data_Types_Practice {
+
+@Test
+public void testing() {
+	
+	
+	
+	
+	double mydbl = 80.1;
+	int myint = (int)mydbl;
+	System.out.println(myint);
+	
+	
+	String mystr = "hello mr";
+	System.out.println(mystr+"\thow are you");
+	
+	
+	
+	
+	
+}
+	
+	
+}
